135e857c8f2cc97a859ed24c0d02e24135678f29
waqasaya-z <waqasayaz@icloud.com>
2023-08-13 00:41:02 +0500

    Added NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ function App() {
   return (
     <>
      <Grid templateAreas={{ 
-      base: `"nav nav" "aside main"`,
+      base: `"nav" "main"`,
       lg: `"nav nav" "aside main"` // 1024 pixels+
     }} >
       <GridItem area='nav' bg='coral'> Nav </GridItem>
       <Show above='lg'> 
        {/* // will be rendered for large screen and above  */}
-      <GridItem area='aside' bg='gold'> Aide </GridItem>
+      <GridItem area='aside' bg='gold'> Aside </GridItem>
       </Show>
   
       <GridItem area='main' bg='dodgerblue'> Main </GridItem>
