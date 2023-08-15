3c1f89a8f0f46486b58c04e23cf870b6fcd5fd76
waqasaya-z <waqasayaz@icloud.com>
2023-08-16 01:34:06 +0500

    added search bar functionality

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,34 @@
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import { BsSearch } from 'react-icons/bs'
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { useRef } from "react";
+import { BsSearch } from "react-icons/bs";
 
 
-const searchInput = () => {
-  return (
-    <InputGroup>
-        <InputLeftElement children={<BsSearch />} />
-        <Input borderRadius={20} placeholder='Search games' variant='filled' />
-
-    </InputGroup>
-
-    
-  )
+interface Props {
+    onSearch: (searchText: string) => void
 }
 
-export default searchInput
+const SearchInput = ({onSearch} : Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  return (
+    <form 
+      style={{ width: '100%'}}
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value)
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20} 
+          placeholder="Search games"
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
+  );
+};
+
+export default SearchInput;
