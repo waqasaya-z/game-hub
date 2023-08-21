fbb91b1250f6cf3bcbf0c006aefc8046af7f967a
waqasaya-z <waqasayaz@icloud.com>
2023-08-22 00:20:19 +0500

    extended react query

--- src/Hooks/usePlatforms.ts
+++ src/Hooks/usePlatforms.ts
@@ -1,11 +1,22 @@
 import platform from "../Data/platform";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
-// interface Platform {
-//     id: number;
-//     name: string;
-//     slug: string;
-// }
+interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
 
- const usePlatform = () => ({data: platform, isLoading: false, error: null})
+//  const usePlatform = () => ({data: platform, isLoading: false, error: null})
+ const usePlatform = () => {
+    return useQuery({
+        queryKey: ['platforms'],
+        queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
+         staleTime: 24 * 60 * 6 * 1000, //24 hours
+         initialData: {count: platform.length, results: platform}
+    })
+ }
 
  export default usePlatform;
