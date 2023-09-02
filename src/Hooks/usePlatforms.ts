0ac976b1dfc4165428d2c2b6f2fb4f7c11998e0c
waqasaya-z <waqasayaz@icloud.com>
2023-09-03 02:32:34 +0500

    Refactor entities

--- src/Hooks/usePlatforms.ts
+++ src/Hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import platform from "../Data/platform";
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import ms from 'ms';
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 
 //  const usePlatform = () => ({data: platform, isLoading: false, error: null})
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
