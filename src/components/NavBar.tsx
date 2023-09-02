cb1e5270af0efded0078d45ade7848755be50dca
waqasaya-z <waqasayaz@icloud.com>
2023-09-03 02:23:52 +0500

    Fixed NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,13 @@ import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/react.svg';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 
 const NavBar = () => {
   return (
     <HStack padding='10px'>
-         <Image src={logo} boxSize='60px' />
+       <Link to='/'>  <Image src={logo} boxSize='60px' /> </Link> 
          <SearchInput />
        <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,10 +2,13 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore(s => s.setSearchText);
+
+ const navigate = useNavigate();
 
   return (
     <form 
@@ -13,6 +16,7 @@ const SearchInput = () => {
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value)
+        navigate('/');
       }}
     >
       <InputGroup>
